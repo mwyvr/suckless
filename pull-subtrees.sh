e4c9cfc76f3ea582384956f1c814c52b37481623
Mike Watkins <git@mikewatkins.ca>
2024-02-11 14:21:39 -0800

    manage subtrees

---
+++ pull-subtrees.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git subtree pull --prefix dwm git://git.suckless.org/dwm master --squash
---
+++ push-subtrees.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git subtree push --prefix dwm git://git.suckless.org/dwm master --squash
